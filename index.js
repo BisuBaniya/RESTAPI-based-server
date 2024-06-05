ac21e510ce96ff022126f1f562c485da65bfda40
BisuBaniya <bisusiddharth1806@gmail.com>
2024-06-05 16:47:29 +0530

    5 June Commit

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require('express');
 
+const users = require('./MOCK_DATA.json');
+
 const app = express();
 
 const PORT = 5000;
 
+//Routes
+
+app.get('/users',(req,res)=>{
+    const html = `
+    <ul>
+        ${users.map((user)=> `<li>${user.first_name}</li>`).join('')}
+    </ul>
+    `
+    res.send(html);
+})
+
+//REST API
+
+app.get('/api/users',(req,res)=>{
+    return res.json(users);
+})
+
+app.get('/api/users/:id',(req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=> user.id === id);
+    if(user){
+        return res.json(user);
+    }else{
+        return res.status(404).json({message:'User not found'});
+    }
+})
+
+
 app.listen(PORT,()=>{
     console.log(`Server Started at PORT: ${PORT}`)
 })
+
+
 
